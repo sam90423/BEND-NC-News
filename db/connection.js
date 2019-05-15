2b7c935cb19f8032eb2793a15a43c1b18e2acf11
sam wood <samwood.5@outlook.com>
2019-05-15 14:41:10 +0100

    Made improvements with my heroku hosting

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,9 @@
-const dbConfig = require("../knexfile");
+const ENV = process.env.NODE_ENV || "development";
+const knex = require("knex");
+console.log(ENV);
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
 
-module.exports = require("knex")(dbConfig);
+module.exports = knex(dbConfig);
